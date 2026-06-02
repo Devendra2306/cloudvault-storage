5b1241f958fca4d67e982e75d0912159d656b31e
Devendra2306 <devdiwakar27@gmail.com>
2026-06-02 22:40:30 +0530

    Fix production configuration and Render deployment

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use(helmet());
 // CORS configuration
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(',')
-  : ['http://localhost:3000', 'http://localhost:3001'];
+  : ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173'];
 
 app.use(cors({
   origin: (origin, callback) => {
